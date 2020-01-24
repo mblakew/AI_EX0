419905f5a33f3e70aebf106f26c6491e677b7ac9
Blake Williams <michaelbwilliams100@gmail.com>
2020-01-24 03:04:16 -0500

    Making progress

--- E0-Base/Data Structures/LinkedList.h
+++ E0-Base/Data Structures/LinkedList.h
@@ -3,50 +3,128 @@
 // LinkedList class should go in the "ufl_cap4053::fundamentals" namespace!
 namespace ufl_cap4053 { namespace fundamentals {
         template <typename T>
+
         class LinkedList{
-        public:
-            class Iterator;
+            struct Node{
+                T data;
+                struct Node *next;
+                struct Node *prev;
+            };
 
-            T operator*() const;
-            //Return the element at the iterator's current position in the queue.
-            Iterator& operator++();
-            //Pre-increment overload; advance the operator one position in the list. Return this iterator. NOTE: if the iterator has reached the end of the list (past the last element), ita should be equal to LinkedList<T>::end().
-            bool operator==(Iterator const& rhs);
-            //Returns true it both iterators point to the same node in the list, and false otherwise.
-            bool operator!=(Iterator const& rhs);
-            //Returns false it both iterators point to the same node in the list, and true otherwise.
+            Node* head;
+            Node* tail;
 
-            LinkedList<T>() {
-                root = nullptr;
-            }
-            //        This is the constructor for LinkedList.
+            public:
+                LinkedList<T>() {
+                    head = nullptr;
+                    tail = nullptr;
+                }
 
-            Iterator begin() const{
-                return Iterator(root);
-            }
-            //        Returns an Iterator pointing to the beginning of the list.
-            Iterator end() const;
-            //        Returns an Iterator pointing past the end of the list (an invalid, unique state).
-            bool isEmpty() const;
-            //        Returns true if there are no elements, false otherwise.
-            T getFront() const;
-            //    Returns the first element in the list.
-            T getBack() const;
-            //    Returns the last element in the list.
-            void enqueue(T element);
-            //    Inserts the specified element to the list.
-            void dequeue();
-            //        Removes the first element from the list.
-            void pop();
-            //        Removes the last element from the list.
-            void clear();
-            //        Removes all elements from the list.
-            bool contains(T element) const;
-            //        Returns true if you find a node whose data equals the specified element, false otherwise.
-            void remove(T element);
-            //        Removes the first node you find whose data equals the specified element.
-            void *root;
-        };
+                class Iterator;
+
+
+                //        This is the constructor for LinkedList.
+
+                Iterator begin() const{
+                    return Iterator(head);
+                }
+                //        Returns an Iterator pointing to the beginning of the list.
+                Iterator end() const{
+                    return Iterator(nullptr);
+                }
+                //        Returns an Iterator pointing past the end of the list (an invalid, unique state).
+
+            class Iterator{
+                    public:
+
+                        T operator*() const{
+                            return current->data;
+                        }
+
+                        //Return the element at the iterator's current position in the queue.
+                        Iterator& operator++(){
+                            Iterator iterator = *this;
+                            ++*this;
+                            return iterator;
+                        }
+                        //Pre-increment overload; advance the operator one position in the list. Return this iterator. NOTE: if the iterator has reached the end of the list (past the last element), ita should be equal to LinkedList<T>::end().
+                        bool operator==(Iterator const& rhs){
+                            return current == rhs.current;
+                        }
+                        //Returns true it both iterators point to the same node in the list, and false otherwise.
+                        bool operator!=(Iterator const& rhs){
+                            return current != rhs.current;
+                        }
+                        //Returns false it both iterators point to the same node in the list, and true otherwise.
+                    private:
+                        const Node* current;
+                };
+
+                bool isEmpty() const{
+                    return head == nullptr;
+                }
+                //        Returns true if there are no elements, false otherwise.
+                T getFront() const{
+                    return head;
+                }
+                //    Returns the first element in the list.
+                T getBack() const{
+                    return tail;
+                }
+                //    Returns the last element in the list.
+                void enqueue(T element){
+                    Node *toInsert = new Node;
+                    toInsert->data = element;
+                    if(head != nullptr) {
+                        head->prev = toInsert;
+                        toInsert->next = head;
+                        return;
+                    }
+
+                    toInsert->next = nullptr;
+                    toInsert->prev = nullptr;
+                    head = toInsert;
+                }
+                //    Inserts the specified element to the list.
+                void dequeue(){
+                    //if the head is not null, and the next is not null, there
+                    //are at least 2 elements in the list, so we must detach
+                    //the old head and set it to the second element, newHead
+                    if(head != nullptr && head->next != nullptr){
+                        Node *newHead = head->next;
+                        newHead->prev = nullptr;
+                        head = newHead;
+                    }
+                    else
+                        head = nullptr;
+                }
+                //        Removes the first element from the list.
+                void pop(){
+                    //if head is null, then there is nothing in the list
+                    //if head is not null, but tail is null there is 1 element
+                    //if both head and tail are not null, then there are 2 elements
+                    if(head == nullptr)
+                        return head;
+                    else if(head != nullptr && tail == nullptr){
+                        return head;
+                    }
+                    else{
+                        //create new tail, unlink with old tail, set tail to new tail
+                        Node *newTail = tail->prev;
+                        newTail->next = nullptr;
+                        tail = newTail;
+                    }
+                }
+                //        Removes the last element from the list.
+                void clear();
+                //        Removes all elements from the list.
+                bool contains(T element) const;
+                //        Returns true if you find a node whose data equals the specified element, false otherwise.
+                void remove(T element);
+                //        Removes the first node you find whose data equals the specified element.
+            };
+
+
 
 
 
--- E0-Base/LinkedList/main.cpp
+++ E0-Base/LinkedList/main.cpp
@@ -43,178 +43,178 @@ int main()
         valueList.enqueue(testStrings[index]);
         pointerList.enqueue(testStrings[index].c_str());
         cout << "\tpointerList:";
-        traverse(pointerList, printCString);
+//        traverse(pointerList, printCString);
         cout << endl;
     }
-
-    // Test dequeue.
-    cout << endl << "Testing dequeue..." << endl;
-    for (;;)
-    {
-        cout << "Removing the front element:";
-        printCString(pointerList.getFront());
-        pointerList.dequeue();
-        cout << endl << "\tpointerList:";
-        traverse(pointerList, printCString);
-        cout << endl;
-
-        if (pointerList.isEmpty())
-            break;
-        else
-            valueList.dequeue();
-    }
-
-    // Test removal of only element.
-    cout << endl << "Removing the only element from valueList:";
-    printCString(valueList.getFront().c_str());
-    valueList.remove(testStrings[numOfStrings - 1]);
-    cout << endl << "\tvalueList:";
-    traverse(valueList, printString);
-    cout << endl;
-
-    // Test containment check when empty
-    cout << endl << "Testing search for: alpha";
-    tempStr = "alpha";
-    if (!valueList.contains(tempStr))
-    {
-        cout << endl << "\tvalueList:";
-        traverse(valueList, printString);
-        cout << endl;
-    }
-
-    // Test pop.
-    cout << endl << "Testing pop..." << endl;
-    cout << "Rebuilding list." << endl;
-    for (unsigned int index = 0; index < numOfStrings; ++index)
-        pointerList.enqueue(testStrings[index].c_str());
-
-    cout << "\tpointerList:";
-    traverse(pointerList, printCString);
-    cout << endl;
-
-    while (!pointerList.isEmpty())
-    {
-        cout << "Removing the back element:";
-        printCString(pointerList.getBack());
-        pointerList.pop();
-        cout << endl << "\tpointerList:";
-        traverse(pointerList, printCString);
-        cout << endl;
-    }
-
-    // Test enqueue after removal.
-    cout << endl << "Testing enqueue after removal..." << endl;
-    for (unsigned int index = 1; index < numOfStrings; ++index)
-    {
-        valueList.enqueue(testStrings[index]);
-        pointerList.enqueue(testStrings[index].c_str());
-    }
-    cout << "\tpointerList:";
-    traverse(pointerList, printCString);
-    cout << endl;
-
-    // Test unsuccessful search.
-    cout << endl << "Testing unsuccessful search and remove:";
-
-    if (pointerList.contains(testStrings[0].c_str()))
-    {
-        assert(valueList.contains(testStrings[0]) && "alpha not found in valueList");
-        cout << " " << testStrings[0];
-        pointerList.remove(testStrings[0].c_str());
-        valueList.remove(testStrings[0]);
-        valueList.remove(testStrings[0]);
-        cout << endl << "\tpointerList:";
-        traverse(pointerList, printCString);
-        cout << endl;
-    }
-    else
-    {
-        assert(!valueList.contains(tempStr) && "alpha found in valueList");
-        cout << " ELEMENT NOT FOUND." << endl;
-    }
-
-    // Test removal of first element.
-    cout << endl << "Testing search and remove:";
-
-    if (pointerList.contains(testStrings[1].c_str()))
-    {
-        assert(valueList.contains(testStrings[1]) && "bravo not found in valueList");
-        cout << " " << testStrings[1];
-        pointerList.remove(testStrings[1].c_str());
-        valueList.remove(testStrings[1]);
-        valueList.remove(testStrings[1]);
-        cout << endl << "\tpointerList:";
-        traverse(pointerList, printCString);
-        cout << endl;
-    }
-    else
-    {
-        assert(!valueList.contains(tempStr) && "bravo found in valueList");
-        cout << " ELEMENT NOT FOUND." << endl;
-    }
-
-    // Test removal of middle element.
-    cout << "Testing search and remove:";
-
-    if (pointerList.contains(testStrings[4].c_str()))
-    {
-        assert(valueList.contains(testStrings[4]) && "dog not found in valueList");
-        cout << " " << testStrings[4];
-        pointerList.remove(testStrings[4].c_str());
-        valueList.remove(testStrings[4]);
-        valueList.remove(testStrings[4]);
-        cout << endl << "\tpointerList:";
-        traverse(pointerList, printCString);
-        cout << endl;
-    }
-    else
-    {
-        assert(!valueList.contains(tempStr) && "dog found in valueList");
-        cout << " ELEMENT NOT FOUND." << endl;
-    }
-
-    // Test removal of last element.
-    cout << "Testing search and remove:";
-
-    if (pointerList.contains(testStrings[7].c_str()))
-    {
-        assert(valueList.contains(testStrings[7]) && "golf not found in valueList");
-        cout << " " << testStrings[7];
-        pointerList.remove(testStrings[7].c_str());
-        valueList.remove(testStrings[7]);
-        valueList.remove(testStrings[7]);
-        cout << endl << "\tpointerList:";
-        traverse(pointerList, printCString);
-        cout << endl;
-    }
-    else
-    {
-        assert(!valueList.contains(tempStr) && "golf found in valueList");
-        cout << " ELEMENT NOT FOUND." << endl;
-    }
-
-    // Test removal of all elements by method.
-    cout << endl << "Removing all elements from pointerList...";
-    pointerList.clear();
-    cout << endl << "\tpointerList:";
-    traverse(pointerList, printCString);
-    cout << endl;
-
-    // Test destructor.
-    cout << endl << "Adding elements to test descructor...";
-    pointerList.enqueue("hotel");
-    pointerList.enqueue("india");
-    pointerList.enqueue("juliet");
-
-    cout << endl << "\tpointerList:";
-    traverse(pointerList, printCString);
-    cout << endl << "\tvalueList:";
-    traverse(valueList, printString);
-    cout << endl << endl;
-
-    cout << "Press ENTER to continue..." << endl;
-    while(cin.get() != '\n') {;}
-    return 0;
+//
+//    // Test dequeue.
+//    cout << endl << "Testing dequeue..." << endl;
+//    for (;;)
+//    {
+//        cout << "Removing the front element:";
+//        printCString(pointerList.getFront());
+//        pointerList.dequeue();
+//        cout << endl << "\tpointerList:";
+//        traverse(pointerList, printCString);
+//        cout << endl;
+//
+//        if (pointerList.isEmpty())
+//            break;
+//        else
+//            valueList.dequeue();
+//    }
+//
+//    // Test removal of only element.
+//    cout << endl << "Removing the only element from valueList:";
+//    printCString(valueList.getFront().c_str());
+//    valueList.remove(testStrings[numOfStrings - 1]);
+//    cout << endl << "\tvalueList:";
+//    traverse(valueList, printString);
+//    cout << endl;
+//
+//    // Test containment check when empty
+//    cout << endl << "Testing search for: alpha";
+//    tempStr = "alpha";
+//    if (!valueList.contains(tempStr))
+//    {
+//        cout << endl << "\tvalueList:";
+//        traverse(valueList, printString);
+//        cout << endl;
+//    }
+//
+//    // Test pop.
+//    cout << endl << "Testing pop..." << endl;
+//    cout << "Rebuilding list." << endl;
+//    for (unsigned int index = 0; index < numOfStrings; ++index)
+//        pointerList.enqueue(testStrings[index].c_str());
+//
+//    cout << "\tpointerList:";
+//    traverse(pointerList, printCString);
+//    cout << endl;
+//
+//    while (!pointerList.isEmpty())
+//    {
+//        cout << "Removing the back element:";
+//        printCString(pointerList.getBack());
+//        pointerList.pop();
+//        cout << endl << "\tpointerList:";
+//        traverse(pointerList, printCString);
+//        cout << endl;
+//    }
+//
+//    // Test enqueue after removal.
+//    cout << endl << "Testing enqueue after removal..." << endl;
+//    for (unsigned int index = 1; index < numOfStrings; ++index)
+//    {
+//        valueList.enqueue(testStrings[index]);
+//        pointerList.enqueue(testStrings[index].c_str());
+//    }
+//    cout << "\tpointerList:";
+//    traverse(pointerList, printCString);
+//    cout << endl;
+//
+//    // Test unsuccessful search.
+//    cout << endl << "Testing unsuccessful search and remove:";
+//
+//    if (pointerList.contains(testStrings[0].c_str()))
+//    {
+//        assert(valueList.contains(testStrings[0]) && "alpha not found in valueList");
+//        cout << " " << testStrings[0];
+//        pointerList.remove(testStrings[0].c_str());
+//        valueList.remove(testStrings[0]);
+//        valueList.remove(testStrings[0]);
+//        cout << endl << "\tpointerList:";
+//        traverse(pointerList, printCString);
+//        cout << endl;
+//    }
+//    else
+//    {
+//        assert(!valueList.contains(tempStr) && "alpha found in valueList");
+//        cout << " ELEMENT NOT FOUND." << endl;
+//    }
+//
+//    // Test removal of first element.
+//    cout << endl << "Testing search and remove:";
+//
+//    if (pointerList.contains(testStrings[1].c_str()))
+//    {
+//        assert(valueList.contains(testStrings[1]) && "bravo not found in valueList");
+//        cout << " " << testStrings[1];
+//        pointerList.remove(testStrings[1].c_str());
+//        valueList.remove(testStrings[1]);
+//        valueList.remove(testStrings[1]);
+//        cout << endl << "\tpointerList:";
+//        traverse(pointerList, printCString);
+//        cout << endl;
+//    }
+//    else
+//    {
+//        assert(!valueList.contains(tempStr) && "bravo found in valueList");
+//        cout << " ELEMENT NOT FOUND." << endl;
+//    }
+//
+//    // Test removal of middle element.
+//    cout << "Testing search and remove:";
+//
+//    if (pointerList.contains(testStrings[4].c_str()))
+//    {
+//        assert(valueList.contains(testStrings[4]) && "dog not found in valueList");
+//        cout << " " << testStrings[4];
+//        pointerList.remove(testStrings[4].c_str());
+//        valueList.remove(testStrings[4]);
+//        valueList.remove(testStrings[4]);
+//        cout << endl << "\tpointerList:";
+//        traverse(pointerList, printCString);
+//        cout << endl;
+//    }
+//    else
+//    {
+//        assert(!valueList.contains(tempStr) && "dog found in valueList");
+//        cout << " ELEMENT NOT FOUND." << endl;
+//    }
+//
+//    // Test removal of last element.
+//    cout << "Testing search and remove:";
+//
+//    if (pointerList.contains(testStrings[7].c_str()))
+//    {
+//        assert(valueList.contains(testStrings[7]) && "golf not found in valueList");
+//        cout << " " << testStrings[7];
+//        pointerList.remove(testStrings[7].c_str());
+//        valueList.remove(testStrings[7]);
+//        valueList.remove(testStrings[7]);
+//        cout << endl << "\tpointerList:";
+//        traverse(pointerList, printCString);
+//        cout << endl;
+//    }
+//    else
+//    {
+//        assert(!valueList.contains(tempStr) && "golf found in valueList");
+//        cout << " ELEMENT NOT FOUND." << endl;
+//    }
+//
+//    // Test removal of all elements by method.
+//    cout << endl << "Removing all elements from pointerList...";
+//    pointerList.clear();
+//    cout << endl << "\tpointerList:";
+//    traverse(pointerList, printCString);
+//    cout << endl;
+//
+//    // Test destructor.
+//    cout << endl << "Adding elements to test descructor...";
+//    pointerList.enqueue("hotel");
+//    pointerList.enqueue("india");
+//    pointerList.enqueue("juliet");
+//
+//    cout << endl << "\tpointerList:";
+//    traverse(pointerList, printCString);
+//    cout << endl << "\tvalueList:";
+//    traverse(valueList, printString);
+//    cout << endl << endl;
+//
+//    cout << "Press ENTER to continue..." << endl;
+//    while(cin.get() != '\n') {;}
+//    return 0;
 }
 
 template <typename T, typename F>
